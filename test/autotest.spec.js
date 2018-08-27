ae834c20e9942e368c3144e7aaefbf4b37f8a0d6
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-08-27 19:51:46 +0300

    Update and rename case.spec.js to autotest.spec.js

--- test/autotest.spec.js
+++ test/autotest.spec.js
@@ -1,34 +1,33 @@
-const should = require('should');
+require('should');
 const puppeteer = require('puppeteer');
 let page;
 let browser;
-const width = 800;
-const height = 600;
+const [ width, height] = [600, 350];
 before(async () => {
   browser = await puppeteer.launch({
-	waitUntil: 'domcontentloaded',
+	  waitUntil: 'domcontentloaded',
     headless: false,
-    slowMo: 30,  
-	devtools: false,  
-    args: [`--window-size=${width},${height}`, `--window-position=30,160`] 
+    slowMo: 30,
+	  devtools: false,
+    args: [`--window-size=${width},${height}`, `--window-position=30,160`]
   });
   page = await browser.newPage();
   await page.setViewport({ width, height });
 });
 
-after(async () => { await browser.close(); }); // в демонстрационных целях закомментить чтобы окно осталось
+// after(async () => { await browser.close(); }); // в демонстрационных целях закомментить чтобы окно осталось
 
 const cases = [
-    {s: 'abc', xs: 'CBA'}, 
+    {s: 'abc', xs: 'CBA'},
     {s: 'zyx', xs: 'XYZ'},
 ];
 
-const URL = 'https://kodaktor.ru/g/autocase';        
-     
+const URL = 'https://kodaktor.ru/g/autocase';
+
 cases.forEach(({ s, xs }) =>
     describe(xs + ' asyncREV()', () => {
       it('respond with revers', async () => {
-        await page.goto(URL);  
+        await page.goto(URL);
         page.evaluate(s => document.querySelector('#inp').value = s, s);
 
         await page.$eval('#button_do', el => el.click());
